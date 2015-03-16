12d6553d19091b7e0cd5d613200af62b902e466d
Stanislav <gonlliuk@gmail.com>
2015-03-16 21:25:03 +0500

    tiny update

--- BatmanVersusBombs.js
+++ BatmanVersusBombs.js
@@ -1,168 +1,65 @@
 /*global print, readline */
-var inputs = readline().split(' ');
-var W = parseInt(inputs[0], 10); // width of the building.
-var H = parseInt(inputs[1], 10); // height of the building.
-var N = parseInt(readline(), 10); // maximum number of turns before game over.
-var inputs = readline().split(' ');
-var X0 = parseInt(inputs[0], 10);
-var Y0 = parseInt(inputs[1], 10);
-var maxW = W, minW = 0;
-var maxH = H, minH = 0;
-var prevState = ' ';
-var BOMB_DIR = readline(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
-while (true) {
-    switch (BOMB_DIR) {
-    case 'U':
-        maxH = Y0 - 1;
-        if (prevState === 'U' && W !== 1) {
-            Y0 -= 1;
-        }
-        Y0 -= Math.round((maxH - minH) / 2);
-        if (Math.round((maxH - minH) / 2) === 0) {
-            Y0 -= 1;
-        }
-        if (Y0 < 0) {
-            Y0 = 0;
-        }
-        print(X0 + ' ' + Y0);
-        prevState = 'U';
-        break;
-    case 'D':
-        minH = Y0 + 1;
-        if (prevState === 'D' && W !== 1) {
-            Y0 += 1;
-        }
-        Y0 += Math.round((maxH - minH) / 2);
-        if (Math.round((maxH - minH) / 2) === 0) {
-            Y0 += 1;
-        }
-        if (Y0 >= H) {
-            Y0 = H - 1;
-        }
-        print(X0 + ' ' + Y0);
-        prevState = 'D';
-        break;
-    case 'R':
-        minW = X0 + 1;
-        if (prevState === 'R') {
-            X0 += 1;
-        }
-        X0 += Math.round((maxW - minW) / 2);
-        if (X0 >= W) {
-            X0 = W - 1;
-        }
-        print(X0 + ' ' + Y0);
-        prevState = 'R';
-        break;
-    case 'L':
-        maxW = X0 - 1;
-        if (prevState === 'L') {
-            X0 -= 1;
-        }
-        X0 -= Math.round((maxW - minW) / 2);
-        if (Math.round((maxW - minW) / 2) === 0) {
-            X0 -= 1;
-        }
-        if (X0 < 0) {
-            X0 = 0;
+(function () {
+    'use strict';
+    var inputs = readline().split(' '),
+        W = parseInt(inputs[0], 10), // width of the building.
+        H = parseInt(inputs[1], 10), // height of the building.
+        N = parseInt(readline(), 10), // maximum number of turns before game over.
+        X0 = 0,
+        Y0 = 0,
+        maxW = W - 1,
+        minW = 0,
+        maxH = H - 1,
+        minH = 0,
+        BOMB_DIR = ''; // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+    inputs = readline().split(' ');
+    X0 = parseInt(inputs[0], 10);
+    Y0 = parseInt(inputs[1], 10);
+    while (N) {
+        BOMB_DIR = readline();
+        switch (BOMB_DIR) {
+        case 'U':
+            Y0 = maxH = Y0 - 1;
+            Y0 -= Math.round((maxH - minH) / 2);
+            break;
+        case 'D':
+            Y0 = minH = Y0 + 1;
+            Y0 += Math.round((maxH - minH) / 2);
+            break;
+        case 'R':
+            X0 = minW = X0 + 1;
+            X0 += Math.round((maxW - minW) / 2);
+            break;
+        case 'L':
+            X0 = maxW = X0 - 1;
+            X0 -= Math.round((maxW - minW) / 2);
+            break;
+        case 'UR':
+            Y0 = maxH = Y0 - 1;
+            X0 = minW = X0 + 1;
+            X0 += Math.round((maxW - minW) / 2);
+            Y0 -= Math.round((maxH - minH) / 2);
+            break;
+        case 'DR':
+            Y0 = minH = Y0 + 1;
+            X0 = minW = X0 + 1;
+            X0 += Math.round((maxW - minW) / 2);
+            Y0 += Math.round((maxH - minH) / 2);
+            break;
+        case 'UL':
+            Y0 = maxH = Y0 - 1;
+            X0 = maxW = X0 - 1;
+            X0 -= Math.round((maxW - minW) / 2);
+            Y0 -= Math.round((maxH - minH) / 2);
+            break;
+        case 'DL':
+            Y0 = minH = Y0 + 1;
+            X0 = maxW = X0 - 1;
+            X0 -= Math.round((maxW - minW) / 2);
+            Y0 += Math.round((maxH - minH) / 2);
+            break;
         }
         print(X0 + ' ' + Y0);
-        break;
-    case 'UR':
-        maxH = Y0 - 1;
-        minW = X0 + 1;
-        if (prevState === 'UR') {
-            Y0 -= 1;
-            X0 += 1;
-        }
-        X0 += Math.round((maxW - minW) / 2);
-        Y0 -= Math.round((maxH - minH) / 2);
-        if (Math.round((maxH - minH) / 2) === 0) {
-            Y0 -= 1;
-        }
-        if (Math.round((maxW - minW) / 2) === 0) {
-            X0 += 1;
-        }
-        if (X0 >= W) {
-            X0 = W - 1;
-        }
-        if (Y0 < 0) {
-            Y0 = 0;
-        }
-        print(X0 + ' ' + Y0);
-        prevState = 'UR';
-        break;
-    case 'DR':
-        minH = Y0 + 1;
-        minW = X0 + 1;
-        if (prevState === 'DR') {
-            X0 += 1;
-            Y0 += 1;
-        }
-        X0 += Math.round((maxW - minW) / 2);
-        Y0 += Math.round((maxH - minH) / 2);
-        if (Math.round((maxH - minH) / 2) === 0) {
-            Y0 += 1;
-        }
-        if (Math.round((maxW - minW) / 2) === 0) {
-            X0 += 1;
-        }
-        if (X0 >= W) {
-            X0 = W - 1;
-        }
-        if (Y0 >= H) {
-            Y0 = H - 1;
-        }
-        prevState = 'DR';
-        print(X0 + ' ' + Y0);
-        break;
-    case 'UL':
-        maxH = Y0 - 1;
-        maxW = X0 - 1;
-        if (prevState === 'UL') {
-            X0 -= 1;
-            Y0 -= 1;
-        }
-        X0 -= Math.round((maxW - minW) / 2);
-        Y0 -= Math.round((maxH - minH) / 2);
-        if (Math.round((maxH - minH) / 2) === 0) {
-            Y0 -= 1;
-        }
-        if (Math.round((maxW - minW) / 2) === 0) {
-            X0 -= 1;
-        }
-        if (X0 < 0) {
-            X0 = 0;
-        }
-        if (Y0 < 0) {
-            Y0 = 0;
-        }
-        print(X0 + ' ' + Y0);
-        prevState = 'UL';
-        break;
-    case 'DL':
-        minH = Y0 + 1;
-        maxW = X0 - 1;
-        if (prevState === 'DL') {
-            X0 -= 1;
-            Y0 += 1;
-        }
-        X0 -= Math.round((maxW - minW) / 2);
-        Y0 += Math.round((maxH - minH) / 2);
-        if (Math.round((maxH - minH) / 2) === 0) {
-            Y0 += 1;
-        }
-        if (Math.round((maxW - minW) / 2) === 0) {
-            X0 -= 1;
-        }
-        if (X0 < 0) {
-            X0 = 0;
-        }
-        if (Y0 >= H) {
-            Y0 = H - 1;
-        }
-        print(X0 + ' ' + Y0);
-        prevState = 'DL';
-        break;
+        N -= 1;
     }
-}
+}());
